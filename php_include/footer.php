876bc9821d0bdff661509223e69b65dd931b734b
fihofficial <sethb@fireandicehosting.com>
2020-03-28 16:22:43 -0400

    updates

--- php_include/footer.php
+++ php_include/footer.php
@@ -1,75 +1,149 @@
 <!--Footer Area-->
+
 	<footer class="footer-area section-padding">
+
 		<div class="container">
+
 			<div class="row">
+
 				<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+
 					<div class="single-footer">
+
 						<img src="assets/images/logo.png" alt="">
+
 						<p>Folly was these three and songs arose whose. Of in vicinity contempt together in possible
+
 							branched.</p>
+
 						<div class="social-media">
+
 							<a href=""><i class="fa fa-facebook"></i></a>
+
 							<a href=""><i class="fa fa-twitter"></i></a>
+
 						</div>
+
 					</div>
+
 				</div>
+
 				<div class="col-lg-2 col-md-6 col-sm-12 col-xs-12">
+
 					<div class="single-footer">
+
 						<h3>Quick Link</h3>
+
 						<div class="footer-menu">
+
 							<ul>
+
 								<li><a href="./index">Home</a></li>
+
 								<li><a href="./about">About</a></li>
+
 								<li><a href="./domain">Domain</a></li>
+
 								<li><a href="./cloudhosting">Hosting</a></li>
+
 								<li><a href="./index">Security</a></li>
+
 								<li><a href="./blog">Blog</a></li>
+
 							</ul>
+
 						</div>
+
 					</div>
+
 				</div>
+
 				<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+
 					<div class="single-footer footer-subscriber">
+
 						<h3>Subscriber</h3>
+
 						<div class="newsletter">
+
 							<p>Subscribe to Newsletter</p>
+
 							<form action="#">
+
 								<input type="email" placeholder="Type your email" required>
+
 								<button type="submit"><i class="fa fa-paper-plane-o"></i></button>
+
 							</form>
+
 						</div>
+
 						<p>More info comming soon</p>
+
 					</div>
+
 				</div>
+
 				<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+
 					<div class="single-footer">
+
 						<h3>Contact Us</h3>
+
 						<div class="footer-contact-info">
+
 							<div class="single-footer-info">
+
 								<i class="fa fa-map-marker"></i>
-								<p>47 Edgebrook Ln, br / Danville br / WV. 25053</p>
+
+								<p>47 Edgebrook Ln, <br>Danville <br>WV. 25053</p>
+
 							</div>
+
 							<div class="single-footer-info">
+
 								<i class="fa fa-phone"></i>
+
 								<p>(681) 238 9458</p>
+
 							</div>
+
 							<div class="single-footer-info">
+
 								<i class="fa fa-envelope"></i><a href="mailto:">support@fireandicehosting.com</a>
+
 							</div>
+
 						</div>
+
 					</div>
+
 				</div>
+
 			</div>
+
 		</div>
+
 	</footer>
+
 	<!--/Footer Area-->
 
+
+
 	<div class="copyright-text">
+
 		<div class="container">
+
 			<div class="row justify-content-center">
+
 				<div class="col centered">
+
 					<p>&copy; <script>document.write(new Date().getFullYear())</script> All Rights Reserved&reg; &mdash; <a href="https://fireandicehosting.com">Fire & Ice Hosting&trade;</a></p>
+
 				</div>
+
 			</div>
+
 		</div>
+
 	</div>
