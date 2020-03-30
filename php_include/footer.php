dd7de5c43f14b302028584be753dc04e1aef44e6
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2020-03-29 21:51:36 -0400

    Update footer.php

--- php_include/footer.php
+++ php_include/footer.php
@@ -18,9 +18,9 @@
 
 						<div class="social-media">
 
-							<a href=""><i class="fa fa-facebook"></i></a>
+							<a href=""><i class="fab fa-facebook"></i></a>
 
-							<a href=""><i class="fa fa-twitter"></i></a>
+							<a href=""><i class="fab fa-twitter"></i></a>
 
 						</div>
 
