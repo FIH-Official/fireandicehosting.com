b7030846fdec0c8b9259efef49791ab92aa321d3
FIH-Official <61325796+FIH-Official@users.noreply.github.com>
2020-04-06 19:34:08 -0400

    s

--- contact.php
+++ contact.php
@@ -42,10 +42,10 @@
 									<input type="email" id="email" name="email" placeholder="Your email address"
 										required>
 								</div>
-								<!--<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+								<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 									<input type="text" id="number" name="number" placeholder="Your phone number"
 										required>
-								</div>-->
+								</div>
 							</div>
 							<textarea name="message" id="message" rows="6" name="message"
 								placeholder="Message"></textarea>
