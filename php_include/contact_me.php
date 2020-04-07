ccaf6202271964604a4899fdab716c7346381453
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2020-04-07 00:04:23 -0400

    Update contact_me.php

--- php_include/contact_me.php
+++ php_include/contact_me.php
@@ -14,11 +14,12 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 // Create the email and send the message
-$to = 'ceo-hunter@hunterswebdesigns.tk';// 'ceo-hunter@hunterswebdesigns.tk']; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Hunter's Web Designs Contact Form:  $name";
-$headers = "From:no-reply@hunterswebdesigns.tk";
+$to = 'support@hunterswebdesigns.tk';// 'ceo-hunter@hunterswebdesigns.tk']; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "FIH Contact Form:  $name";
+$headers = "From:no-reply@fireandicehosting.com";
 $headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+// $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$headers .= "Reply-To: $email_address";
 
 
 if(mail($to,$email_subject,$email_body,$headers))
