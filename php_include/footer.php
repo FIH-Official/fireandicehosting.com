1bb6d7d0144a985c10109ed0c2b4585d3c69bc0c
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2020-04-06 22:47:43 -0400

    Update footer.php

--- php_include/footer.php
+++ php_include/footer.php
@@ -36,15 +36,15 @@
 								<li><a href="./index">Home</a></li>
 
 								<li><a href="./about">About</a></li>
-
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
 								<li class="nav-item dropdown submenu"><a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Hosting <i class="fas-fa-angle-down"></i></a>
-								
-								</li>
 								<ul class="dropdown-menu">
 									<li class="nav-item"><a href="./minecraft-hosting" class="nav-link">Minecraft Hosting</a></li>
 									<li class="nav-item"><a href="./fx-hosting" class="nav-link">FiveM Hosting</a></li>
 									<li class="nav-item"><a href="./vps-hosting" class="nav-link">VPS</a></li>
 								</ul>
+								</li>
+								</div>
 								<li class="nav-item"><a href="./contact" class="nav-link">Contact Us</a></li>
 								<li class="nav-item"><a href="./account" class="nav-link">Account</a></li>
 							</ul>
