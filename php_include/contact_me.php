f7a840de15904681842accef1d074c10652e57a0
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2020-04-07 00:02:02 -0400

    Update contact_me.php

--- php_include/contact_me.php
+++ php_include/contact_me.php
@@ -1,35 +1,33 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['number'])    ||
-   empty($_POST['text'])	||
+if(empty($_POST['name'])          ||
+   empty($_POST['email'])         ||
+   empty($_POST['message'])    ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "Please fill out the form!";
-	return false;
+   echo "<script>alert('No arguments provided! Please Provide Arguments and Submit Again.');</script>";
+   echo "<script>document.location.href='./contact'</script>";
+    return false;
    }
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$number = $_POST['number'];
-$text = $_POST['text'];
-
+$message = $_POST['message'];
 // Create the email and send the message
-$o = 'contact@fireandicehosting.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$to = 'support@hunterswebdesigns.tk';
-$email_subject = "FIH Contact Form:  $name";
-$email_body = "You have received a new message from FIH's contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\n Number: $number \n\n Message:\n$text";
-$headers = "From: no-reply@fireandicehosting.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-// mail($to,$email_subject,$email_body,$headers);
-// return true;
+$to = 'ceo-hunter@hunterswebdesigns.tk';// 'ceo-hunter@hunterswebdesigns.tk']; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Hunter's Web Designs Contact Form:  $name";
+$headers = "From:no-reply@hunterswebdesigns.tk";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
 if(mail($to,$email_subject,$email_body,$headers))
 {
-	echo "<script>alert('Success!:\n<?php echo $message;?>');</script>";
+	echo "<script>alert('Success!:\nThe email was sent!');</script>";
     echo "<script>document.location.href='./contact'</script>";
 }
 
 return true;
     
+
 ?>
