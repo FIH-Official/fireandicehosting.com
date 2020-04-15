8298c7821c1c532e5de530ee36220c69f939c0a5
FIH-Official <61325796+FIH-Official@users.noreply.github.com>
2020-04-14 22:45:06 -0400

    up

--- team.php
+++ team.php
@@ -38,117 +38,9 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="0.4s">
-					<div class="single-team">
-						<img src="assets/images/team1.png" alt="">
-						<h4>Seth Blevins</h4>
-						<p>Ceo & Founder</p>
-						<div class="team-social">
-							<a href=""><i class="fab fa-discord"></i></a>
-							<a href=""><i class="fab fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="0.5s">
-					<div class="single-team">
-						<img src="assets/images/team2.png" alt="">
-						<h4>Hunter Lancaster</h4>
-						<p>CDO</p>
-						<div class="team-social">
-							<a href=""><i class="fab fa-facebook"></i></a>
-							<a href=""><i class="fab fa-twitter"></i></a>
-							<a href="https://discord.gg/9g8FBNc"><i class="fab fa-discord"></i></a>
-							<a href="https://hunterswebdesigns.tk"><i class="fa fa-drivers-license"></i></a>
-						</div>
-					</div>
-				</div>
-			<!--
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="0.6s">
-					<div class="single-team">
-						<img src="assets/images/team3.png" alt="">
-						<h4>Adan Pagana</h4>
-						<p>Ceo & Founder</p>
-						<div class="team-social">
-							<a href=""><i class="fa fa-facebook"></i></a>
-							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-dribbble"></i></a>
-							<a href=""><i class="fa fa-pinterest"></i></a>
-							<a href=""><i class="fa fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="0.7s">
-					<div class="single-team">
-						<img src="assets/images/team4.png" alt="">
-						<h4>Adan Pagana</h4>
-						<p>Ceo & Founder</p>
-						<div class="team-social">
-							<a href=""><i class="fa fa-facebook"></i></a>
-							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-dribbble"></i></a>
-							<a href=""><i class="fa fa-pinterest"></i></a>
-							<a href=""><i class="fa fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="0.8s">
-					<div class="single-team">
-						<img src="assets/images/team5.png" alt="">
-						<h4>Adan Pagana</h4>
-						<p>Ceo & Founder</p>
-						<div class="team-social">
-							<a href=""><i class="fa fa-facebook"></i></a>
-							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-dribbble"></i></a>
-							<a href=""><i class="fa fa-pinterest"></i></a>
-							<a href=""><i class="fa fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="0.9s">
-					<div class="single-team">
-						<img src="assets/images/team6.png" alt="">
-						<h4>Adan Pagana</h4>
-						<p>Ceo & Founder</p>
-						<div class="team-social">
-							<a href=""><i class="fa fa-facebook"></i></a>
-							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-dribbble"></i></a>
-							<a href=""><i class="fa fa-pinterest"></i></a>
-							<a href=""><i class="fa fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="1s">
-					<div class="single-team">
-						<img src="assets/images/team7.png" alt="">
-						<h4>Adan Pagana</h4>
-						<p>Ceo & Founder</p>
-						<div class="team-social">
-							<a href=""><i class="fa fa-facebook"></i></a>
-							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-dribbble"></i></a>
-							<a href=""><i class="fa fa-pinterest"></i></a>
-							<a href=""><i class="fa fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6 col-sm-12 wow fadeInLeft" data-wow-delay="1.1s">
-					<div class="single-team">
-						<img src="assets/images/team8.png" alt="">
-						<h4>Adan Pagana</h4>
-						<p>Ceo & Founder</p>
-						<div class="team-social">
-							<a href=""><i class="fa fa-facebook"></i></a>
-							<a href=""><i class="fa fa-twitter"></i></a>
-							<a href=""><i class="fa fa-dribbble"></i></a>
-							<a href=""><i class="fa fa-pinterest"></i></a>
-							<a href=""><i class="fa fa-linkedin"></i></a>
-						</div>
-					</div>
-				</div>
-			</div>
+			<?php
+			require_once ("./php_include/team.php");
+			?>
 		</div>
 	</section>-->
 	<!--/Team Area-->
@@ -225,7 +117,7 @@
 
 	<!--Testimonial Area-->
 	<?php
-		require ("./php_includes/testimonials.php");
+    require_once ("./php_include/testimonial.php");
 	?>
 	<!--/Testimonial Area-->
 
