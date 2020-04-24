e2484df397724bd154084b6427ffb236b5b9016d
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2020-04-24 12:31:27 -0400

    Update contact_me.php

--- php_include/contact_me.php
+++ php_include/contact_me.php
@@ -134,10 +134,21 @@ $json_data = json_encode([
             
         ]
     ]
-])
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    
+$ch = curl_init($webhookurl);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
+$response = curl_exec($ch);
+// if you need to debug, or find out why you can't send message uncomment line below, and execute script,
+// echo $response
 
-
+curl_close($ch);
 //================================================================================//
 // This is the end of discord webhook code                                        //
 //================================================================================//
