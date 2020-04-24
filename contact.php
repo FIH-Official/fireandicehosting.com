47a245f95e3e193024e7775c1a8733d3fc494a79
Hunter L <38705200+OwnerHunter@users.noreply.github.com>
2020-04-24 12:18:08 -0400

    Added new features to the site
    Added a discord intergrated webhook for the contact form.

--- contact.php
+++ contact.php
@@ -1,90 +1,97 @@
 <!doctype html>
 <html lang="en">
-
+<?php require_once ("./php_include/hidden/api.php");?>
 <?php require_once ("./php_include/header.php");?>
+<script src="https://www.google.com/recaptcha/api.js"></script>
 
 <body>
-<?php require_once ("./php_include/nav-bar.php");?>
+    <?php require_once ("./php_include/nav-bar.php");?>
 
-	<!--Custom Hero Area-->
-	<section class="custom-hero">
-		<div class="container">
-			<div class="row cl-white">
-				<div class="col-lg-8 col-md-6">
-					<div class="custom-hero-title">
-						<h2>Contact us</h2>
-						<div class="custom-hero-breadcrumb">
-							<ul>
-								<li><a href="index">Home</a></li>
-								<li>Contact</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<img src="assets/images/reliable.png" alt="">
-				</div>
-			</div>
-		</div>
-	</section>
-	<!--/Custom Hero Area-->
+    <!--Custom Hero Area-->
+    <section class="custom-hero">
+        <div class="container">
+            <div class="row cl-white">
+                <div class="col-lg-8 col-md-6">
+                    <div class="custom-hero-title">
+                        <h2>Contact us</h2>
+                        <div class="custom-hero-breadcrumb">
+                            <ul>
+                                <li><a href="index">Home</a></li>
+                                <li>Contact</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <img src="assets/images/reliable.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/Custom Hero Area-->
 
-	<!--Contact Area-->
-	<section class="contact-area pb-0 section-padding">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
-					<div class="contact-form">
-						<form action="./php_include/contact_me.php" method="POST" id="contact_form_submit">
-							<input type="text" id="name" name="name" placeholder="Your name" required>
-							<div class="row">
-								<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-									<input type="email" id="email" name="email" placeholder="Your email address"
-										required>
-								</div>
-								<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-									<input type="text" id="number" name="number" placeholder="Your phone number"
-										required>
-								</div>
-							</div>
-							<textarea name="message" id="message" rows="6" name="message"
-								placeholder="Message"></textarea>
-							<button class="bttn btn-fill" type="submit">Send Message</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!--/Contact Area-->
+    <!--Contact Area-->
+    <section class="contact-area pb-0 section-padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
+                    <div class="contact-form">
+                        <form action="./php_include/contact_me.php" method="POST" id="contact_form_submit">
+                            <input type="text" id="name" name="name" placeholder="Your name" required>
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                    <input type="number" name="discordid" id="discordid"
+                                        placeholder="please place your discord user id here if you have a discord account">
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                    <input type="email" id="email" name="email" placeholder="Your email address"
+                                        required>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                    <input type="text" id="phonenumb" name="phonenumb" placeholder="Your phone number"
+                                        required>
+                                </div>
+                            </div>
+                            <textarea name="message" id="message" rows="6" name="message"
+                                placeholder="Message"></textarea>
+                            <div class="g-recaptcha" data-sitekey="<?php echo SITE_KEY; ?>"></div>
+                            <div id="mail-status"></div>
+                            <button class="bttn btn-fill" type="submit">Send Message</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/Contact Area-->
 
-	<!--Contact Map-->
-	<section class="contact-area section-padding">
-	<!--	<div id="map" class="map-area"></div> -->
-	</section>
-	<!--/Contact Map-->
+    <!--Contact Map-->
+    <section class="contact-area section-padding">
+        <!--	<div id="map" class="map-area"></div> -->
+    </section>
+    <!--/Contact Map-->
 
-	<?php require_once ("./php_include/footer.php"); ?>
-
-
+    <?php require_once ("./php_include/footer.php"); ?>
 
 
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="assets/js/jquery-3.2.1.min.js"></script>
-	<script src="assets/js/jquery-migrate.js"></script>
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="assets/js/popper.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
-	<script src="assets/js/owl.carousel.min.js"></script>
 
-	<script src="assets/js/waypoints.min.js"></script>
-	<script src="assets/js/jquery.counterup.min.js"></script>
-	<script src="assets/js/scrollUp.min.js"></script>
-	<script src="assets/js/wow.min.js"></script>
-	<script src="assets/js/magnific-popup.min.js"></script>
-	<script src="assets/js/contact.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBi4wppP2kn3BHzI99FMCwl-wyss7Sh6qs"></script>
-	<script src="assets/js/script.js"></script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <script src="assets/js/jquery-migrate.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/scrollUp.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/magnific-popup.min.js"></script>
+    <script src="assets/js/contact.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBi4wppP2kn3BHzI99FMCwl-wyss7Sh6qs"></script>
+    <script src="assets/js/script.js"></script>
 </body>
 
 </html>
--- php_include/contact_me.php
+++ php_include/contact_me.php
@@ -1,9 +1,12 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])          ||
-   empty($_POST['email'])         ||
-   empty($_POST['message'])    ||
+if(empty($_POST['name'])||
+   empty($_POST['email'])||
+   empty($_POST['message'])||
+   empty($_POST['discordid'])||
+   empty($_POST['phonenumb'])||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   !filter_var($_POST['phonenumb'], FILTER_VALIDATE_PHONE)
    {
    echo "<script>alert('No arguments provided! Please Provide Arguments and Submit Again.');</script>";
    echo "<script>document.location.href='./contact'</script>";
@@ -13,15 +16,131 @@ if(empty($_POST['name'])          ||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-// Create the email and send the message
+$phone = $_POST['phonenumb'];
+//====================================
+// the discord id is for the embed webhook 
+// along with the support role id
+//====================================
+$discord = $_POST['discordid'];
+$supportid = "supportrolehere";
+// ====================================
+// Website images for the embeded message 
+// =====================================
+$img_url = "https://fireandicehostingtesting.ml/assets/images/logo.png";
+$icon_url = "https://fireandicehostingtesting.ml/assets/images/icon.png";
+$webhook_url = "https://ptb.discordapp.com/api/webhooks/703278438991003718/feqaJoCmYChbTJzD_OldyHw4TTpbJpM_LMHaOQ51BLpHCHX1FeG3aJucby6uKKj1Ne5y";
+$website_url = "https://fireandicehosting.com"
+// Create the  and send the message
 $to = 'support@hunterswebdesigns.tk';// 'ceo-hunter@hunterswebdesigns.tk']; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "FIH Contact Form:  $name";
 $headers = "From:no-reply@fireandicehosting.com";
 $headers .= "MIME-Version: 1.0\r\n";
 // $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 $headers .= "Reply-To: $email_address";
+// reCaptcha security
+//reCAPTCHA validation
+$SECRET_KEY = "6LerxO0UAAAAALGZDsbzPjFUYoBIT_SB8XU5WXZr";
+if (isset($_POST['g-recaptcha-response'])) {
+		
+    require('component/recaptcha/src/autoload.php');		
+    
+    $recaptcha = new \ReCaptcha\ReCaptcha($SECRET_KEY);
+
+    $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+      if (!$resp->isSuccess()) {
+            $output = json_encode(array('type'=>'error', 'text' => '<b>Captcha</b> Validation Required!'));
+            die($output);				
+      }	
+}
+//================================================================================//
+// This here is the discord webhook code                                          //
+//================================================================================//
+$timestamp = date('c', strtotime("now"));
+
+$json_data = json_encode([
+    //Message
+    "content" => "**ATTENTION** <@&$supportid> | There is a new contact form, details are listed below.",
+    //Username
+    "username" => "$name",
+    // avatar url.
+    // uncomment to replace image set in webhook
+    // "avatar_url" => "$icon_url",
+    // Text-To-Speach
+    "tts" => false,
+    //File Upload
+    // "file" => "",
+    // Embeds Array
+    "embeds" => [
+        [
+            //Embed Title
+            "title" => "New Contact From By: $name",
+            //Embed Type
+            "type" => "rich",
+            // Embed Description
+            "description" => "$name with the discord account of <@$discord> has created a new support form",
+            // URL of title link 
+            "url" => "$websiteurl",
+            // Timestamp of embed must be formatted as ISO8601 
+            "timestamp"=>"$timestamp",
+            // Footer 
+            "footer" => [
+                "text" => "Fire & Ice Hosting™",
+                "icon_url" => "$icon_url"
+            ],
+            // Image to send
+            "image" => [
+                "url" => "$img_url"
+            ],
+            // Thumbnail
+            "thumbnail" => [
+                "url"=>"$icon_url"
+            ],
+            // Author
+            "author" => [
+                "name" => "$name",
+                "url" => "$website_url"
+            ],
+            // Additional Fields Array 
+            "fields" => [
+                // field one
+                [
+                    "name" => "Contactor:",
+                    "value" => "$name",
+                    "inline"=> false
+                ],
+                [
+                    "name" => "Discord Account:",
+                    "value" => "<@$discord>",
+                    "inline"=> true
+                ],
+                [
+                    "name" => "Users Email:",
+                    "value" => "**[$email](<https://mailto:$email>)**",
+                    "inline"=> true
+                ],
+                [
+                    "name" => "Phone #",
+                    "value" => "**[$phone](<https://tel:$phone>)",
+                    "inline"=> true
+                ],
+                [
+                    "name" => "Message Content:",
+                    "value" => "**```$message```**",
+                    "inline"=> false
+                ]
+                // Etc... 
+            ]
+            
+        ]
+    ]
+])
 
 
+
+//================================================================================//
+// This is the end of discord webhook code                                        //
+//================================================================================//
 if(mail($to,$email_subject,$email_body,$headers))
 {
 	echo "<script>alert('Success!:\nThe email was sent!');</script>";
